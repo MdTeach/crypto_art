4999d7542ae88f257f0aeb24a7955ebbf037b5d0
Abishek Bashyal <abishek_bashyal@outlook.com>
2021-06-05 20:20:27 +0545

    generate button added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,36 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 interface ImageRes {
   image: string;
 }
 
 function App() {
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState('');
+
+  const fetchImage = async () => {
+    const response = await axios.get('http://127.0.0.1:5000/');
+    const imageData: ImageRes = response.data;
+    setImage(imageData.image);
+  };
 
   useEffect(() => {
-    (async () => {
-      const response = await axios.get("http://127.0.0.1:5000/");
-      const imageData: ImageRes = response.data;
-      setImage(imageData.image);
-      // let img = new ImageData(imageData.image);
-    })();
+    console.log('Effect call');
+    fetchImage();
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">Abishek Bashyal</header>
-      {image !== "" ? (
+    <div className="App" style={{textAlign: 'center'}}>
+      <h2 className="App-header">Crypto Art</h2>
+      {image !== '' ? (
         <img
           src={`data:image/jpeg;base64,${image}`}
           alt="generator"
-          style={{ width: "300px", height: "300px" }}
+          style={{width: '200px', height: '200px'}}
         />
       ) : null}
+      <br />
+      <button onClick={fetchImage}>Generate Another</button>
     </div>
   );
 }
