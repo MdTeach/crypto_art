84a11b1e61a6c67a2e449a0a0b9a9b2de817190a
Abishek Bashyal <abishek_bashyal@outlook.com>
2021-06-14 11:33:43 +0545

    token mint test done

--- test/ArtNFT.test.js
+++ test/ArtNFT.test.js
@@ -2,17 +2,42 @@ const assert = require('assert');
 const ArtNFT = artifacts.require('ArtNFT.sol');
 
 contract('Token Deployment', async (accounts) => {
+  const zero_address = '0x0000000000000000000000000000000000000000';
   let contract;
 
   before(async () => {
     contract = await ArtNFT.deployed();
   });
 
-  it('Token Deployment', async () => {
-    const [expectedName, expectedToken] = ['CryptoArt', 'CART'];
-    const tokenName = await contract.name();
-    const tokenSymbol = await contract.symbol();
-    assert.strictEqual(tokenName, expectedName, 'Token name not matching');
-    assert.strictEqual(tokenSymbol, expectedToken, 'Token symbol not matching');
+  describe.skip('Token Deployment', () => {
+    it('has correct name & token', async () => {
+      const [expectedName, expectedToken] = ['CryptoArt', 'CART'];
+      const tokenName = await contract.name();
+      const tokenSymbol = await contract.symbol();
+      assert.strictEqual(tokenName, expectedName, 'Token name not matching');
+      assert.strictEqual(
+        tokenSymbol,
+        expectedToken,
+        'Token symbol not matching',
+      );
+    });
+  });
+
+  describe('Token Minting', () => {
+    it('can be minted', async () => {
+      const tokenURI =
+        'ipfs://ipfs.io/ipfs/bafybeieu5yrwl3agtl4tfinendjmpw4blsf4wps7g7knrwj22vr3xxagmy';
+      const {logs} = await contract.createCollectible(tokenURI);
+      const {from, to, tokenId} = logs[0].args;
+      console.log('event');
+      assert.strictEqual(
+        logs[0].event,
+        'Transfer',
+        'Tansfer Event not invoked',
+      );
+      assert.strictEqual(from, zero_address, 'Origin must from zero address');
+      assert.strictEqual(to, accounts[0]);
+      assert.strictEqual(tokenId.toNumber(), 0, 'First token is zero');
+    });
   });
 });
