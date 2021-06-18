ee8807a96052a340b8aff28a077aac16e85c4cec
Abishek Bashyal <abishek_bashyal@outlook.com>
2021-06-18 13:25:18 +0545

    code:web3 and home componet done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,16 @@
-import React, {useEffect, useState} from 'react';
 import useWeb3 from './hooks/web3';
 
-import axios from 'axios';
-
-interface ImageRes {
-  image: string;
-}
+import GenerateLayout from './componets/GenerateArt';
 
 function App() {
-  const [image, setImage] = useState('');
+  const {isLoading, isWeb3, web3, accounts} = useWeb3();
 
-  const fetchImage = async () => {
-    const response = await axios.get('http://127.0.0.1:5000/');
-    const imageData: ImageRes = response.data;
-    setImage(imageData.image);
-  };
-
-  useEffect(() => {
-    console.log('Effect call');
-    fetchImage();
-  }, []);
-
+  if (isLoading) {
+    return <h1>Loading ..</h1>;
+  }
   return (
-    <div className="App" style={{textAlign: 'center'}}>
-      <h2 className="App-header">Crypto Art</h2>
-      {image !== '' ? (
-        <img
-          src={`data:image/jpeg;base64,${image}`}
-          alt="generator"
-          style={{width: '175px', height: '175px'}}
-        />
-      ) : null}
-      <br />
-      <button onClick={fetchImage}>Generate Another</button>
+    <div className="App">
+      <GenerateLayout />
     </div>
   );
 }
---
+++ client/src/componets/GenerateArt.tsx
@@ -0,0 +1,38 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+interface ImageRes {
+  image: string;
+}
+
+function GenerateArtLayout() {
+  const [image, setImage] = useState('');
+
+  const fetchImage = async () => {
+    const response = await axios.get('http://127.0.0.1:5000/');
+    const imageData: ImageRes = response.data;
+    setImage(imageData.image);
+  };
+
+  useEffect(() => {
+    console.log('Effect call');
+    fetchImage();
+  }, []);
+
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h2>Crypto Art</h2>
+      {image !== '' ? (
+        <img
+          src={`data:image/jpeg;base64,${image}`}
+          alt="generator"
+          style={{width: '175px', height: '175px'}}
+        />
+      ) : null}
+      <br />
+      <button onClick={fetchImage}>Generate Another</button>
+    </div>
+  );
+}
+
+export default GenerateArtLayout;
