5587418dca02781b3ddd88085ef5812d5cf488b0
Abishek Bashyal <abishek_bashyal@outlook.com>
2021-06-23 19:53:45 +0545

    code:item list for salle transaction works

--- client/src/componets/token_sales/sell_layout.tsx
+++ client/src/componets/token_sales/sell_layout.tsx
@@ -11,21 +11,82 @@ interface Props {
   token_id: string;
 }
 
-const SellLayout = ({token_id}: Props) => {
-  const context = useContext(Web3Context);
+const ETHER = Math.pow(10, 18);
 
-  const {metadata, sellingPrice, owner, isLoading} = useTokenInfo({
+const SellLayout = ({token_id}: Props) => {
+  const [price, setPrice] = useState(0.001);
+  const context = useContext(Web3Context);
+  const history = useHistory();
+
+  const {metadata, isLoading} = useTokenInfo({
     token_id,
     context,
   });
+
+  const handleCancel = () => {
+    history.push(`/detail/${token_id}`);
+  };
+
+  const handleSale = async () => {
+    if (price === 0) {
+      alert('Cannot sale for zeor price');
+      return;
+    }
+    const valueWei = price * ETHER;
+    var txn = await context.nftContract?.methods
+      .approve(context.tradeContractAddress, token_id)
+      .send({from: context.account});
+
+    console.log(txn);
+
+    var txn = await context.tradeContract?.methods
+      .listForSale(token_id, valueWei)
+      .send({from: context.account});
+
+    console.log(txn);
+    console.log('success');
+  };
 
   if (isLoading) {
     console.log('Loading....');
   } else {
     console.log(metadata);
-    console.log(owner);
   }
-  return <div>{isLoading ? <h3>Loading...</h3> : <h1>Token for sale</h1>}</div>;
+  return (
+    <div>
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <>
+          {/* <h4>Sell</h4>
+          <ul>
+            <li>Approve the contract to sale</li>
+            <li>Place the token for sale</li>
+          </ul> */}
+          <div style={{display: 'flex', padding: '1em'}}>
+            <img
+              alt={metadata?.name}
+              src={metadata?.image}
+              style={{width: 70, height: 70}}
+            />
+            <p>Token Id: {metadata?.token_id}</p>
+            <p>{metadata?.name}</p>
+          </div>
+          <input
+            type="number"
+            value={price}
+            name="price"
+            onChange={(e) => setPrice(parseInt(e.target.value))}
+          />
+          <label>Ether</label>
+          <br />
+          <br />
+          <button onClick={handleSale}>List on sale</button>
+          <button onClick={handleCancel}>Cancel</button>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default SellLayout;
