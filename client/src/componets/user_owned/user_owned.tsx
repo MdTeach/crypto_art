ea95f9504a0e3f80155150bc87d0acaeb343ab01
Abishek Bashyal <abishek_bashyal@outlook.com>
2021-06-25 19:23:15 +0545

    style:user owned done

--- client/src/componets/user_owned/user_owned.tsx
+++ client/src/componets/user_owned/user_owned.tsx
@@ -4,12 +4,33 @@ import assert from 'assert';
 import Web3Context from '../../contexts/Web3Context';
 import {MetaDataIndexed} from '../../utils/MetaData';
 import {Link} from 'react-router-dom';
-
 import Loading from '../helpers/loading';
+
+import React from 'react';
+import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
+import Grid, {GridSpacing} from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }),
+);
 
 type UserTokens = Array<string>;
 
 function OwnedLayout() {
+  const classes = useStyles();
+
   const [ownedTokens, setOwnedTokens] = useState<UserTokens>([]);
   const [data, setData] = useState<MetaDataIndexed[]>([]);
   const [isFetching, setIsFetching] = useState(true);
@@ -50,7 +71,10 @@ function OwnedLayout() {
 
   // get the meta data of tokens
   useEffect(() => {
-    if (isEmpty) return;
+    if (isEmpty) {
+      setIsFetching(false);
+      return;
+    }
     (async () => {
       const [datas, err] = await loadMetaData(ownedTokens);
       if (err) {
@@ -67,25 +91,61 @@ function OwnedLayout() {
     return <Loading />;
   }
   if (isEmpty) {
-    return <h1>Empty</h1>;
+    return (
+      <h1 style={{textAlign: 'center', marginTop: '2.5em', fontSize: '3em'}}>
+        No Tokens were found
+        <br /> to your address :(
+      </h1>
+    );
   }
   return (
-    <div style={{textAlign: 'center'}}>
-      <h1>Your nfts</h1>
-      {data.map((el) => (
-        <div key={el.image}>
-          <img src={el.image} alt={el.name} style={{width: 150, height: 150}} />
-          <div>Name: {el.name}</div>
-          <div>Token Id: {el.token_id}</div>
-          <div>Artist: {el.properties.artist}</div>
-          <Link to={`/detail/${el.token_id}`}>View in detail</Link>
-          <br />
-          <br />
-          <br />
-        </div>
-      ))}
+    <div style={{maxWidth: '80%', position: 'relative'}}>
+      <h1 style={{textAlign: 'center', padding: '1em'}}>Your Tokens:</h1>
+      <Grid container className={classes.root} spacing={5}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={5}>
+            {data.map((el) => (
+              <Grid key={el.token_id} item>
+                <div
+                  style={{
+                    textAlign: 'center',
+                    padding: '1em',
+                    border: '2px solid #E5E8EB',
+                    borderRadius: '10px',
+                  }}>
+                  <img
+                    src={el.image}
+                    alt={el.name}
+                    style={{width: 150, height: 150, padding: '10px'}}
+                  />
+                  <div>Name: {el.name}</div>
+                  <div>Token Id: {el.token_id}</div>
+                  <Link to={`/detail/${el.token_id}`}>View in detail</Link>
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
+}
+{
+  /* <div style={{textAlign: 'center'}}>
+  <h1>Your nfts</h1>
+  {data.map((el) => (
+    <div key={el.image}>
+      <img src={el.image} alt={el.name} style={{width: 150, height: 150}} />
+      <div>Name: {el.name}</div>
+      <div>Token Id: {el.token_id}</div>
+      <div>Artist: {el.properties.artist}</div>
+      <Link to={`/detail/${el.token_id}`}>View in detail</Link>
+      <br />
+      <br />
+      <br />
+    </div>
+  ))}
+</div>; */
 }
 
 export default OwnedLayout;
